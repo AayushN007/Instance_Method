eb78006fad98138479486d9bab0054a0fde048cf
AayushN007 <hemanat2004@gmail.com>
2026-06-19 14:44:44 +0530

    Add files via upload

---
+++ Instance_Method.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, n1, u1, m1, a1,):
+        self.name = n1
+        self.usn = u1
+        self.mob = m1
+        self.add = a1
+        
+S1 = Student("Rama", "1VTU001", 1234, "Bang")
+S2 = Student("Sita", "1VTU002", 1223, "Bang")
+S3 = Student("Ravana", "1VTU003", 1235, "Bang")
+
+print(S1.name)
+print(S2.mob)
+print(S3.add)
